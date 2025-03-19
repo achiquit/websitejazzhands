adaef50ee475fca9002fc1c1f4039ce9c0c39166
Andre Chiquito <85582759+achiquit@users.noreply.github.com>
2025-03-19 17:29:32 -0400

    Tile color fanciness
    Made it so that on mobile the tiles are color only when fully in the viewport and on desktop only when hovered over

--- js/script.js
+++ js/script.js
@@ -6,3 +6,26 @@ const toggleMenu = document.querySelector('#toggleMenu');
 toggleMenu.onclick = () => {
   toggleMenu.classList.toggle("hamburger-toggle");
 };
+
+// Mobile tile color scroll function
+
+$(window).on('scroll resize', function() {
+    var element = $('#test'); // Replace '#yourElement' with the actual selector
+    var elementTop = element.offset().top;
+    var elementBottom = elementTop + element.outerHeight();
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    if (elementTop >= viewportTop && elementBottom <= viewportBottom) {
+        // The element is fully visible
+        element.removeClass("grayscale");
+        console.log('Element is fully visible');
+        console.log(element);
+        // Perform actions when the element is fully visible
+    } else {
+        // The element is not fully visible
+        element.addClass("grayscale");
+        console.log('Element is not fully visible');
+        // Perform actions when the element is not fully visible
+    }
+}); 
