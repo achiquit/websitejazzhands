1f6ccccdb7eb97b687d47da7b6188da4d9ad1656
achiquit <andre.chiquito@gmail.com>
2025-05-09 17:13:16 -0400

    youtube playlist update & efficiency updates for python script

--- resources/wild-west/youtube-gen.py
+++ resources/wild-west/youtube-gen.py
@@ -2,13 +2,11 @@
 __author__ = "andre.chiquit.ooo"
 
 from pytube import Playlist
+open('resources/wild-west/favorite_videos.txt', 'w').close()
 
 favorites = Playlist("https://youtube.com/playlist?list=FLsNkuoghnddIhV58-aFWBhw&si=pyp3vspHUoDGcohg")
 
 with open("resources/wild-west/favorite_videos.txt", "w") as vids:
     for i in favorites.videos:
-        vids.write('<iframe src="https://www.youtube.com/embed/')
-        vid_id = i.watch_url.replace("https://youtube.com/watch?v=", "")
-        vids.write(vid_id)
-        vids.write('" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen class="lg:h-120"></iframe>')
-        vids.write('\n')
+        vids.write(i.watch_url.replace("https://youtube.com/watch?v=", "https://www.youtube.com/embed/"))
+        vids.write('" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen class="lg:h-120"></iframe>\n')
