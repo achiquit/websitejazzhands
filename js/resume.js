7b6e3bdd328e49dc1840216ca04d5898d73326ca
Andre Chiquito <85582759+achiquit@users.noreply.github.com>
2026-01-29 17:37:16 -0500

    Automatic DB Update

--- js/resume.js
+++ js/resume.js
@@ -21,7 +21,6 @@ $("#show-references").click(function () {
 })
 // Resume page scroll to top
 var toTopButton = document.getElementById("to-top-button");
-
 // When the user scrolls down 200px from the top of the document, show the button
 window.onscroll = function () {
     if ($(window).width() < 1024) {
@@ -39,3 +38,27 @@ window.onscroll = function () {
 $(".timeline-btn").click(function(){
   $("#timeline").fadeToggle();
 });
+
+// ######## Hide expired certs #############
+
+var certs = [["spi", new Date("Dec 25, 2026 00:00:00").getTime()], ["spg", new Date("Dec 25, 2026 00:00:00").getTime()], ["ymhfr", new Date("Apr 30, 2028 00:00:00").getTime()], ["cpr", new Date("Jan 29, 2028 00:00:00").getTime()]]
+
+// Get today's date and time
+var now = new Date().getTime();
+
+// Hide expired certs
+for (let i = 0; i < certs.length; i++) {
+    var cert = document.getElementById(certs[i][0]);
+    var exp = certs[i][1]
+    if (now > exp) {
+        cert.classList.add("hidden");
+    } else {
+        console.log("Andre is " + certs[i][0] + " certified! Yahoo!")
+    }
+};
+
+
+function myFunction() {
+  var element = document.getElementById("myDIV");
+  element.classList.remove("mystyle");
+}
