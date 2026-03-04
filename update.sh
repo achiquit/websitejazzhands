6ab472df39e9a68e4ce74b949c526144fe1dd9e4
Andre Chiquito <85582759+achiquit@users.noreply.github.com>
2026-03-04 14:43:02 -0500

    Automatic DB Update

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-echo "Updating Sitemap"
+echo "=+=+=+=+=+=+=+=--- Updating Sitemap ---=+=+=+=+=+=+=+="
 python3 -m sitemap-gen
 
-echo "Pushing website to GitHub"
+echo "=+=+=+=+=+=+=+=--- Pushing website to GitHub ---=+=+=+=+=+=+=+="
 git commit -a -m "Automatic DB Update"
 git push
 
-echo "Updating server"
+echo "=+=+=+=+=+=+=+=--- Updating server ---=+=+=+=+=+=+=+="
 read -s -p "Enter Server Password Here: " password
 echo "$password" | tailscale ssh andre@ubuntu "sudo -S ./update.sh"
