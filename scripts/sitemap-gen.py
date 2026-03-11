5fa1e6f1fca4f923dd45f2a7fdd688b0edd71e05
Andre Chiquito <85582759+achiquit@users.noreply.github.com>
2026-03-11 17:38:51 -0400

    Automatic DB Update

--- scripts/sitemap-gen.py
+++ scripts/sitemap-gen.py
@@ -3,7 +3,7 @@ import re
 
 count = 0
 with open("../sitemap.txt","w+") as sitemapConsole:
-        for root, dirs, files in os.walk('../.'):
+        for root, dirs, files in os.walk('.'):
             if "index.html" in files:
                 count += 1
                 if count == 1:
