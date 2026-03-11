2b1d31391c68b804c719e96ced9e4856a5b8a029
Andre Chiquito <85582759+achiquit@users.noreply.github.com>
2026-03-11 17:20:37 -0400

    Automatic DB Update

---
+++ scripts/sitemap-gen.py
@@ -0,0 +1,17 @@
+import os
+import re
+
+count = 0
+with open("../sitemap.txt","w+") as sitemapConsole:
+    with open("../sitemap/sitemap.txt", "w+") as sitemapUser:
+        for root, dirs, files in os.walk('../.'):
+            if "index.html" in files:
+                count += 1
+                if count == 1:
+                    sitemapConsole.write(f"https://andre.chiquit.ooo" + re.sub("^.", "", root, count=1) + "/")
+                    sitemapUser.write(f"Home")
+                else:
+                    sitemapConsole.write(f"\nhttps://andre.chiquit.ooo" + re.sub("^.", "", root, count=1) + "/")
+                    sitemapUser.write(f"\n" + root)
+
+print(f"Total Pages: " + str(count))
