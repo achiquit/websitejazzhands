4c56a2d72637ec43de8ea08d5e220dfea29cbf5b
Andre Chiquito <85582759+achiquit@users.noreply.github.com>
2026-04-01 17:38:07 -0400

    Automatic DB Update

--- js/script.js
+++ js/script.js
@@ -49,6 +49,11 @@ $("#header").load("/header.html", function() {
     }
 });
 
+// Load the footer HTML
+$("#footer").load("/footer.html", function() {
+    footerInit();
+});
+
 // Calculating years since a certain date (for About Me page)
 function yearsSinceDate(dateString) {
   const now = new Date();
@@ -151,4 +156,45 @@ function() {
             element.addClass("grayscale");
         }
     });
+});
+
+// Add current year
+document.getElementById('footer-year').appendChild(document.createTextNode(new Date().getFullYear()))
+
+// Function to show a random line from a txt file every day
+$(document).ready(function() {
+
+    function getDate() {
+        var nowDate = new Date(); 
+        var date = nowDate.getFullYear()+'/'+(nowDate.getMonth()+1)+'/'+nowDate.getDate();
+        return date;
+    }
+
+    function getRandomItem(arr) {
+        
+        var date = getDate();
+        // Use the date as a seed to get a random number
+        var seededNum = new Math.seedrandom(date);
+        // Multiply the random number to be a float large enough to choose among the items in the array 
+        var randomIndex = seededNum() * (arr.length - 1);
+        // Turn the float into an int
+        var chosenOne = Math.trunc(randomIndex);
+        // Return the random item
+        return arr[chosenOne];
+    }
+
+    function randomLineGenerator(filePath, target) {
+        var linesArray = [];
+
+        $.get(filePath, function(data) {
+            // Split the data by new lines and store in the array
+            linesArray = data.split('\n');
+
+            // Randomly choose an item from the array
+            var randomItem = getRandomItem(linesArray);
+            $(target).replaceWith(randomItem);
+        });
+    }
+    
+    randomLineGenerator('/quotes.txt', '#quote');
 });
--- resources/wild-west/youtube-gen.py
+++ resources/wild-west/youtube-gen.py
@@ -1,13 +1,19 @@
 """ Creating a txt list of all video urls of my favorite youtube vidoes :) """
 __author__ = "andre.chiquit.ooo"
 
+import pytube
 from pytube import Playlist
 
 open('resources/wild-west/favorite_videos.txt', 'w').close()
 
 favorites = Playlist("https://youtube.com/playlist?list=FLsNkuoghnddIhV58-aFWBhw&si=pyp3vspHUoDGcohg")
 
-with open("resources/wild-west/favorite_videos.txt", "w") as vids:
+# with open("resources/wild-west/favorite_videos.txt", "w") as vids:
+#     for i in favorites.videos:
+#         vids.write(i.watch_url.replace('https://youtube.com/watch?v=', '<iframe src="https://www.youtube.com/embed/'))
+#         vids.write('" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen class="lg:h-120"></iframe>\n')
+
+with open("resources/wild-west/test.txt", "w") as vids:
     for i in favorites.videos:
         vids.write(i.watch_url.replace('https://youtube.com/watch?v=', '<iframe src="https://www.youtube.com/embed/'))
         vids.write('" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen class="lg:h-120"></iframe>\n')
